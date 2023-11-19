e1907fac2be9bae495f69c68135ccbb0944c61d7
EUCHly <107652218+EUCHly@users.noreply.github.com>
2023-11-19 14:51:01 +0300

    Add files via upload

---
+++ NewBehaviourScript.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("hello PIBLS");
+        print("кто прочитал, тот PIBL!!!!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
